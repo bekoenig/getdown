1f34accebbebf3e9a854a3778b189922e2e52032
Benjamin König <koenig87@googlemail.com>
2024-08-04 07:24:53 +0200

    refactor: removed duplicate import

--- launcher/src/main/java/io/github/bekoenig/getdown/launcher/ProxyPanel.java
+++ launcher/src/main/java/io/github/bekoenig/getdown/launcher/ProxyPanel.java
@@ -17,7 +17,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
